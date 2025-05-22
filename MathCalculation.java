93b9ef3c72970acaf9780f4b0a34ba863d7f1f38
Ganesh Singh <jiganesh963@gmail.com>
2025-05-22 08:40:45 +0545

    Add files via upload
    calculated the value of x 12 line of code

---
+++ MathCalculation.java
@@ -0,0 +1,12 @@
+package com.ganesh.singh;
+
+public class MathCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+	    System.out.println(x);
+
+	}
+
+}
