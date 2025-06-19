c199ddd394e01e9c3e70e84e800a6beb6e0d33cc
Ganpati <you@example.com>
2025-06-19 08:45:17 +0545

    IntegerExample.java

---
+++ JS.java
@@ -0,0 +1,14 @@
+package com.ganesh.singh;
+
+public class MathCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		int Z = x+y;
+	    System.out.println(Z);
+
+	}
+
+}
